ef5bb702fff0c3d85ac227629def1f3112231814
hernangonzalez93 <hernanc.9@hotmail.com>
2026-04-28 21:46:25 +0200

    Add Vehicle entity to manage the domain

--- src/GtMotive.Microservice.Domain/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GtMotive.Microservice.Domain
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ src/GtMotive.Microservice.Domain/Entities/Vehicle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtMotive.Microservice.Domain.Entities;
+
+public class Vehicle
+{
+    /// <summary>
+    /// Identifier for the vehicle.
+    /// </summary>
+    public string Id { get; init; }
+
+    /// <summary>
+    /// Brand of the vehicle.
+    /// </summary>
+    public string Brand { get; set; }
+
+    /// <summary>
+    /// Model of the vehicle.
+    /// </summary>
+    public string Model { get; init; }
+
+    /// <summary>
+    /// Manufacture date of the vehicle.
+    /// </summary>
+    public DateTime ManufactureDate { get; init; }
+
+    /// <summary>
+    ///  Initializes a new instance of the <see cref="Vehicle"/> class.
+    /// </summary>
+    public Vehicle()
+    {
+
+    }
+}
